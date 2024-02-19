e6c2152afd0a881bcba372beba5bbac486cafd35
its-Simy <135281067+its-Simy@users.noreply.github.com>
2024-02-18 21:10:35 -0500

    Final product
    this code shows the problem with shallow copies

--- Car.java
+++ Car.java
@@ -25,7 +25,35 @@ public class Car {
         this.carbrand = other.carbrand;
         this.milage = other.milage;
     }
+    // Getters and setters
+    public String getCartype() {
+        return cartype;
+    }
 
+    public void setCartype(String cartype) {
+        this.cartype = cartype;
+    }
+
+    public String getCarbrand() {
+        return carbrand;
+    }
+
+    public void setCarbrand(String carbrand) {
+        this.carbrand = carbrand;
+    }
+
+    public int getMilage() {
+        return milage;
+    }
+
+    public void setMilage(int milage) {
+        this.milage = milage;
+    }
+
+
+    public void printMe(){
+        System.out.println("Car Type:" +  this.cartype + " Car Brand: " + this.carbrand + " Milage: " + this.milage);
+    }
 
 
 
--- Main.java
+++ Main.java
@@ -1,13 +1,28 @@
 public class Main {
 
-Car one = new Car("EV", "Tesla", 20);
-Car two = new Car("SUV", "Honda", 1000);
-Car three = new Car("Sports Car", "Koenigsegg", 150);
+    public static void main(String[] args) {
+        Car one = new Car("EV", "Tesla", 20);
+        Car two = new Car();
+        Car three = new Car("Sports Car", "Koenigsegg", 150);
 
 //The tester object will show the shallow copy issue
-Car tester = new Car(one);
+        Car tester = new Car();
+        tester = one;
 
 
+        one.printMe();
+        two.printMe();
+        three.printMe();
+        tester.printMe();
 
+        one.setMilage(500);
+        one.setCarbrand("Infinity");
+        one.setCartype("Sedan");
 
+        //This print function shows how the tester object is a shallow copy because without being adjusted directly
+        // it still changes because in the memory it has the same address as the one car object
+        one.printMe();
+        tester.printMe();
+
+    }
 }
